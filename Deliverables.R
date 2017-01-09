801962d1ac87444db090588f8000f7d3a1532140
Marcus <marcus0308@live.hk>
2017-01-09 16:05:40 +0000

    first commit

---
+++ Deliverables.R
@@ -0,0 +1,11 @@
+if(is.na(match(c("devtools"),installed.packages()[,"Package"]))) install.packages(new.packages) else library(devtools)
+suppressMessages(devtools::install_github("marcuskhl/BasicSettings"));suppressMessages(library(BasicSettings))
+
+#~~~Notes~~~#
+# This script extracts outputs from M:\Technology\DATA\Media Management\Demand side\Models\Completed
+
+#~~~Notes~~~#
+
+
+
+
